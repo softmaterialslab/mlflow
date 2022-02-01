425a2a2ff667c177b10d6a114b62103261493a34
kadupitiya <kadupitiya@kadupitiya.lk>
2022-02-01 12:53:56 -0500

    testing shearing for star

--- shearing/shell_scripts/pre.processing.sh
+++ shearing/shell_scripts/pre.processing.sh
@@ -6,6 +6,7 @@ P=0.1	# Pressure
 S=1e10	# strain rate
 F=1000	# move and file dump frequency
 M=T	# T:STAR S=SQL
+U=293 # Temperature
 
 while [ $# -gt 0 ]; do
 
@@ -20,6 +21,7 @@ done
 
 # Print program values
 echo "Pressure = $P"
+echo "Temperature = $U"
 echo "Strain rate = $S"
 echo "Dump frequency = $F"
 
@@ -37,6 +39,7 @@ echo "Selected modecular model = $M"
 # create a LAMMPS input file from the template
 cat ../infiles/in.lammps.template | \
 sed -e 's/USER-P-USER/'$P'/g' | \
+sed -e 's/USER-U-USER/'$U'/g' | \
 sed -e 's/USER-S-USER/'$S'/g' | \
 sed -e 's/USER-F-USER/'$F'/g' | \
 sed -e 's/USER-M-USER/'$M'/g' > ../in.shear
